3ccc2104aef92265354c91eef09bd74f7888c4ac
okseunghwan <pokd7004@naver.com>
2026-02-19 17:18:37 +0900

    Delete js/script.js

--- js/script.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    // trail_tab li a를 클릭하면,
-    $(".trail_tab> li> a").click(function(){
-        //눌려진 board li a(this) 의 부모(li)에 on을 추가하고 형제요소들은 on을 제거한다.
-        $(this).parent().addClass("on").siblings().removeClass("on")
-
-    });
-});
